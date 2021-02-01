f698a2a3c4b254919b4dbe9aaffc747d071fd3a4
Nathan Torquato <n.torquato.silva@gmail.com>
2021-02-01 12:30:03 -0300

    feat: ensure GetRecipesController throws BadRequest if more than 3 ingredients are provided

--- src/useCases/GetRecipes/GetRecipesController.spec.ts
+++ src/useCases/GetRecipes/GetRecipesController.spec.ts
@@ -1,4 +1,5 @@
 import { BadRequest } from '../../errors';
+import { HttpRequest } from '../../protocols';
 import { GetRecipesController } from './GetRecipesController';
 
 interface SutFactory {
@@ -17,8 +18,21 @@ describe('GetRecipesController', () => {
 	test('should throw BadRequest if no ingredients are provided', async () => {
 		const { sut } = makeSut();
 		expect(sut).toBeDefined();
-		const httpRequest: any = {
+		const httpRequest: HttpRequest = {
 			query: {},
+		};
+
+		const promise = sut.handle(httpRequest);
+		await expect(promise).rejects.toThrow(BadRequest);
+	});
+
+	test('should throw BadRequest if more than 3 ingredients are provided', async () => {
+		const { sut } = makeSut();
+		expect(sut).toBeDefined();
+		const httpRequest: HttpRequest = {
+			query: {
+				i: 'onion,tomato,lettuce,orange',
+			},
 		};
 
 		const promise = sut.handle(httpRequest);
--- src/useCases/GetRecipes/GetRecipesController.ts
+++ src/useCases/GetRecipes/GetRecipesController.ts
@@ -2,11 +2,20 @@ import { BadRequest } from '../../errors';
 import { Controller, HttpRequest, HttpResponse } from '../../protocols';
 
 export class GetRecipesController implements Controller {
+	private readonly maximumQtyOfIngredients = 3;
+
 	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-		const ingredients = httpRequest.query.i;
-		if (!ingredients) {
+		const ingredientsString = httpRequest.query.i as string;
+		if (!ingredientsString) {
 			throw new BadRequest(
 				'Request does not match expected pattern (/recipes/?i={ingredient_1},{ingredient_2},{ingredient_3})',
+			);
+		}
+
+		const ingredients = ingredientsString.split(',');
+		if (ingredients.length > this.maximumQtyOfIngredients) {
+			throw new BadRequest(
+				`You can provide from 1 to ${this.maximumQtyOfIngredients} ingredients`,
 			);
 		}
 
