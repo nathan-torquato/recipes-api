2ccc54881d3dab6abfbc8494a4ed731ec5e9dbe9
Nathan Torquato <n.torquato.silva@gmail.com>
2021-02-01 15:43:57 -0300

    refactor: encapsulate Recipe building logic inside GetRecipesUseCase

--- src/useCases/GetRecipes/GetRecipesUseCase.ts
+++ src/useCases/GetRecipes/GetRecipesUseCase.ts
@@ -4,17 +4,23 @@ import { RecipeProvider, GIFProvider } from '../../providers';
 export class GetRecipesUseCase {
 	constructor(private recipeProvider: RecipeProvider, private gifProvider: GIFProvider) {}
 
+	private buildRecipes(rawRecipe: Recipe, gifByTitle): Recipe {
+		return {
+			title: rawRecipe.title,
+			ingredients: rawRecipe.ingredients,
+			link: rawRecipe.link,
+			gif: gifByTitle[rawRecipe.title],
+		};
+	}
+
 	async execute(ingredients: string[]): Promise<Recipe[]> {
 		const recipesWithoutGIF = await this.recipeProvider.getByIngredients(ingredients);
 		const titles = recipesWithoutGIF.map(({ title }) => title);
 		const gifByTitle = await this.gifProvider.getByKeyword(titles);
 
-		const recipes: Recipe[] = recipesWithoutGIF.map(recipe => ({
-			title: recipe.title,
-			ingredients: recipe.ingredients,
-			link: recipe.link,
-			gif: gifByTitle[recipe.title],
-		}));
+		const recipes: Recipe[] = recipesWithoutGIF.map(rawRecipe =>
+			this.buildRecipes(rawRecipe, gifByTitle),
+		);
 
 		return recipes;
 	}
