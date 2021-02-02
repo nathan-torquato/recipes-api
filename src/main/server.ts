11b8d39fae8febd941f7792b249e62461935d8ea
Nathan Torquato <n.torquato.silva@gmail.com>
2021-02-02 00:04:49 -0300

    feat: validate required env vars are set before running server

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,17 @@
+import dotenv from 'dotenv';
 import { app } from './app';
 
+dotenv.config();
+
+const requiredEnvVars = ['RECIPE_PUPPY_API_URL', 'GIF_PUPPY_API_URL', 'GIF_API_KEY'];
+
+requiredEnvVars.forEach(envVar => {
+	if (!process.env[envVar]) {
+		throw new Error(`Environment var ${envVar} is required!`);
+	}
+});
+
 const PORT = 3000;
-app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+	console.log(`Server is running on http://localhost:${PORT}/recipes?i=onion,tomato`),
+);
