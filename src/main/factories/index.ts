57509f7cb73ddd8101f2e905df151d1075b1ed1e
Nathan Torquato <n.torquato.silva@gmail.com>
2021-02-08 19:30:25 -0300

    chore: add dev script for automatic server restart while development

--- src/main/factories/index.ts
+++ src/main/factories/index.ts
@@ -5,14 +5,15 @@ import { GetRecipesController, GetRecipesUseCase } from '../../useCases';
 dotenv.config();
 
 export function makeGetRecipesController(): GetRecipesController {
-	const gifAPIBaseURL = process.env.GIF_PUPPY_API_URL;
-	const gifAPIKey = process.env.GIF_API_KEY;
-	const gifNotFoundURL = process.env.GIF_NOT_FOUND_URL;
+	const gifAPIBaseURL = (process.env.GIF_PUPPY_API_URL as unknown) as string;
+	const gifAPIKey = (process.env.GIF_API_KEY as unknown) as string;
+	const gifNotFoundURL = (process.env.GIF_NOT_FOUND_URL as unknown) as string;
 	const gifProvider = new GiphyGIFProvider(gifAPIBaseURL, gifAPIKey, gifNotFoundURL);
 
-	const recipePuppyApiUrl = process.env.RECIPE_PUPPY_API_URL;
+	const recipePuppyApiUrl = (process.env.RECIPE_PUPPY_API_URL as unknown) as string;
 	const recipeProvider = new RecipePuppyRecipeProvider(recipePuppyApiUrl);
 
 	const getRecipesUseCase = new GetRecipesUseCase(recipeProvider, gifProvider);
+
 	return new GetRecipesController(getRecipesUseCase);
 }
